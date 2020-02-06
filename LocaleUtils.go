53adbaca5b635183586192b57512617816637889
jojiiOfficial <jojii@jojii.de>
2020-02-06 10:07:24 +0100

    improved langParsing

--- LocaleUtils.go
+++ LocaleUtils.go
@@ -3,35 +3,9 @@ package main
 import "golang.org/x/text/language"
 
 func strToTag(stri, mode string) (language.Tag, string) {
-	switch stri {
-	case "en", "english", "englisch":
-		{
-			return language.English, "Translate " + mode + " english"
-		}
-	case "de", "deutsch", "german":
-		{
-			return language.German, "Translate " + mode + " german"
-		}
-	case "es", "spanish", "spanisch":
-		{
-			return language.Spanish, "Translate " + mode + " spanish"
-		}
-	case "pl", "polish", "polnisch":
-		{
-			return language.Polish, "Translate " + mode + " polish"
-		}
-	case "nl", "dutch", "niederlaendisch", "niederländisch":
-		{
-			return language.Dutch, "Translate" + mode + " dutch"
-		}
-	case "zu", "zulu":
-		{
-			return language.Zulu, "Translate" + mode + " zulu"
-		}
-	case "ru", "russian", "russisch":
-		{
-			return language.Russian, "Translate " + mode + " russian"
-		}
+	lang, err := language.Parse(stri)
+	if err != nil {
+		return language.Tag{}, ""
 	}
-	return language.Tag{}, ""
+	return lang, "Translate " + mode + " " + lang.String()
 }
